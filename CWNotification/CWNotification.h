2025bc70b43a0fd507381d1b6a1bdc398158408c
Zitao Xiong <caoer115@gmail.com>
2017-06-25 07:18:40 -0400

    add carthage

---
+++ CWNotification/CWNotification.h
@@ -0,0 +1,19 @@
+//
+//  CWNotification.h
+//  CWNotification
+//
+//  Created by Zitao Xiong on 6/25/17.
+//  Copyright © 2017 Zitao Xiong. All rights reserved.
+//
+
+@import UIKit;
+
+//! Project version number for CWNotification.
+FOUNDATION_EXPORT double CWNotificationVersionNumber;
+
+//! Project version string for CWNotification.
+FOUNDATION_EXPORT const unsigned char CWNotificationVersionString[];
+
+#import <CWNotification/CWStatusBarNotification.h>
+
+
--- CWStatusBarNotification/CWStatusBarNotification.h
+++ CWStatusBarNotification/CWStatusBarNotification.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Cezary Wojcik. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import UIKit;
 
 /**
  * @brief A simple completion used for handling tapping the notification.
